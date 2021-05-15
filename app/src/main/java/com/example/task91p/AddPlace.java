d3d91e0f600ebc4a957acbc2963a7eca4f07bdb0
RUSHITHA PERERA <ldperera@deakin.edu.au>
2021-05-15 18:35:33 +0530

    Setting the UI and Google Maps

---
+++ app/src/main/java/com/example/task91p/AddPlace.java
@@ -0,0 +1,64 @@
+package com.example.task91p;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.Status;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.AutocompleteActivity;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class AddPlace extends AppCompatActivity {
+
+    EditText placeName, placeLocation;
+    Button currentLocation, showOnMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_place);
+
+        placeName = findViewById(R.id.place_name);
+        placeLocation = findViewById(R.id.location_details);
+
+
+        Places.initialize(getApplicationContext(),"");
+
+        placeLocation.setFocusable(false);
+        placeLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                List<Place.Field> fieldList = Arrays.asList(Place.Field.ADDRESS, Place.Field.LAT_LNG, Place.Field.NAME);
+
+                Intent intent = new Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY, fieldList).build(AddPlace.this);
+
+                startActivityForResult(intent, 100);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 100 && resultCode == RESULT_OK){
+            Place place = Autocomplete.getPlaceFromIntent(data);
+            placeLocation.setText(place.getAddress());
+        }
+        else if(resultCode == AutocompleteActivity.RESULT_ERROR){
+            Status status = Autocomplete.getStatusFromIntent(data);
+            Toast.makeText(getApplicationContext(), status.getStatusMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+}
--- app/src/main/java/com/example/task91p/MainActivity.java
+++ app/src/main/java/com/example/task91p/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.task91p;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button addPlace, showAll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addPlace = findViewById(R.id.buttonAdd);
+        showAll = findViewById(R.id.buttonShow);
+
+        addPlace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddPlace.class);
+                startActivity(intent);
+            }
+        });
     }
 }
